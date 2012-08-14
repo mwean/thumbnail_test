828cd533359de3bfaf75f7e6b9877478cdbe08d4
Matthew Wean <matthew.wean@gmail.com>
2012-08-13 20:37:14 -0400

    Updates for m-turk

--- thumbnail_test.rb
+++ thumbnail_test.rb
@@ -5,7 +5,22 @@ require 'coffee-script'
 
 def imgs
   img_paths = Dir['./public/img/*.*'].shuffle
-  img_paths.map { |path| 'img' + path.split('img').last }
+  img_paths.sample(3).map { |path| 'img' + path.split('img').last }
+end
+
+BIG_LIST_OF_IMAGE_SETS = {
+  1 => ['current', 'new_best', 'worst'],
+  2 => ['worst', 'current', 'new_best']
+}
+
+get '/images/:id' do
+  @first, @second, @third = BIG_LIST_OF_IMAGE_SETS[params[:id].to_i]
+  haml :index
+end
+
+get '/choose/:choice' do
+  # Post to Amazon here...
+  "Yay, you picked #{params[:choice]}!"
 end
 
 get '/style.css' do
